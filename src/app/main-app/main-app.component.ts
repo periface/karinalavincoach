5046a7f2bd87c1084b6983da72789269827736bb
Alan Torres <temp_peri@hotmail.com>
2021-12-07 13:36:15 -0600

    main app setup

---
+++ src/app/main-app/main-app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './main-app.component.html',
+})
+export class MainAppComponent {
+  /**
+   *
+   */
+  constructor() {}
+}
