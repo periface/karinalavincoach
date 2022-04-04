11c87e39007164ba3479fa2c9323a3eccf9a7db6
Alan Torres <temp_peri@hotmail.com>
2022-04-04 13:53:34 -0500

    updated database

--- src/app/main-app/excersices/ejercicios/ejercicios.component.ts
+++ src/app/main-app/excersices/ejercicios/ejercicios.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { BaseComponent } from 'src/app/shared/components/base-component.component';
 
@@ -6,13 +7,50 @@ import { BaseComponent } from 'src/app/shared/components/base-component.componen
   templateUrl: './ejercicios.component.html',
   styleUrls: ['./ejercicios.component.scss'],
 })
-export class EjerciciosComponent extends BaseComponent {
+export class EjerciciosComponent extends BaseComponent implements OnInit {
   groups: any = [];
+  categorias: any[] = [];
+  ejercicios: any[] = [];
   /**
    *
    */
   constructor(private afs: AngularFirestore) {
     super();
+  }
+  ngOnInit(): void {
+    // let sub = this.afs
+    //   .collection('categoria-ejercicio')
+    //   .snapshotChanges()
+    //   .pipe(
+    //     map((actions) =>
+    //       actions.map((a) => {
+    //         const data = a.payload.doc.data() as any;
+    //         data['id'] = a.payload.doc.id;
+    //         return { ...data };
+    //       })
+    //     )
+    //   )
+    //   .subscribe((data) => {
+    //     console.log(data);
+    //     this.categorias = data;
+    //     sub.unsubscribe();
+    //   });
+    let exSb = this.afs
+      .collection('ejercicios')
+      .snapshotChanges()
+      .pipe(
+        map((actions) =>
+          actions.map((a) => {
+            const data = a.payload.doc.data() as any;
+            data['id'] = a.payload.doc.id;
+            return { ...data };
+          })
+        )
+      )
+      .subscribe((data) => {
+        this.ejercicios = data;
+        exSb.unsubscribe();
+      });
   }
   checkEx($event: any) {
     let file = $event.target.files[0];
@@ -70,5 +108,39 @@ export class EjerciciosComponent extends BaseComponent {
       result.push(ejercicioObj);
     }
     return result;
+  }
+  async guardarTodo() {
+    for await (const group of this.groups) {
+      let categoria = { name: group.key, id: this.afs.createId() };
+      await this.afs
+        .collection('categoria-ejercicio')
+        .doc(categoria.id)
+        .set(categoria);
+
+      for await (const ej of group.elements) {
+        let ejercicio = {
+          name: ej.Ejercicio,
+          id: this.afs.createId(),
+          idCategoria: categoria.id,
+        };
+        await this.afs
+          .collection('ejercicios')
+          .doc(ejercicio.id)
+          .set(ejercicio);
+        console.log(ejercicio);
+        for await (const zona of ej.zonasMusculares) {
+          let zonaMuscular = {
+            zonaMuscular: zona.key,
+            subZonas: zona.elements,
+            id: this.afs.createId(),
+            ejercicioId: ejercicio.id,
+          };
+          await this.afs
+            .collection('zonas-musculares')
+            .doc(zonaMuscular.id)
+            .set(zonaMuscular);
+        }
+      }
+    }
   }
 }
